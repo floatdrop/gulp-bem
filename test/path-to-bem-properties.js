44ff0c78e5444419b157b201e3d30fefc3f4960b
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-11 00:08:03 +0600

    refactor(tests): fixed test names in path-to-bem-properties.js

--- test/path-to-bem-properties.js
+++ test/path-to-bem-properties.js
@@ -12,11 +12,11 @@ describe('path-to-bem-properties', function () {
         props('/block/__elem/index.js').should.have.property('elem', 'elem');
     });
 
-    it('should extract block', function () {
+    it('should extract block, element and mod', function () {
         props('/block/__elem/_mod/index.js').should.have.property('mod', 'mod');
     });
 
-    it('should extract block', function () {
+    it('should extract block and mod', function () {
         var p = props('/block/_mod/index.js');
         p.should.have.property('block', 'block');
         p.should.have.property('mod', 'mod');
