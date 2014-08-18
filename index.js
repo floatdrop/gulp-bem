fa30f1abcb14e33b1b1a9f6a483bae51328b9cb8
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-18 23:55:39 +0600

    refactor(bem): moved debug to own package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     objects: require('./objects.js'),
     tree: require('./tree.js'),
-    src: require('./src.js'),
-    debug: require('./debug.js'),
+    src: require('./src.js')
 };
