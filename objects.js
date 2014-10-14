0df1fcb187c94cb4d745216df2f149e225dc9d73
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-14 14:45:40 +0600

    Add map helper to objects stream

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var stream = require('bem-object').stream;
 var read = require('multistream');
+var asyncDone = require('async-done');
+var map = require('map-stream');
 
 function objects(levels) {
     if (typeof levels === 'string') { levels = [ levels ]; }
@@ -10,7 +12,20 @@ function objects(levels) {
         return stream(level, this._options);
     });
 
-    return read(streams, { objectMode: true });
+    var outputStream = read(streams, { objectMode: true });
+
+    outputStream.map = function (mapper) {
+        var mapStream = map(function (data, cb) {
+            asyncDone(mapper.bind(null, data), function (error) {
+                if (error) { return cb(error); }
+                cb();
+            });
+        });
+
+        return outputStream.pipe(mapStream);
+    };
+
+    return outputStream;
 }
 
 module.exports = objects;
